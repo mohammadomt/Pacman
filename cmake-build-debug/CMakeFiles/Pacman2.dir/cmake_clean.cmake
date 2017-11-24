dcf601256e78a9d48b0542c66badb0f7290daf5d
Mohammad Omidvar <mohammad.omt99@yahoo.com>
2017-11-24 12:30:43 +0330

    Initializing0

---
+++ cmake-build-debug/CMakeFiles/Pacman2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pacman2.dir/main.c.obj"
+  "Pacman2.pdb"
+  "Pacman2.exe"
+  "Pacman2.exe.manifest"
+  "libPacman2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pacman2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- phase0.c
+++ phase0.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+char table [100][100];
+
+enum MDir {Top = 1, Right = 2, Down = 3, Left =4};
+
+struct Point
+{
+    int X;
+    int Y;
+};
+
+struct Ghost
+{
+    enum GType {Blinky = 0, Pinky = 1, Clyde = 2, Inky= 3} Type;
+    enum Mode {Deff = 0, Off =1 } Mode;
+
+    enum MDir CurrentDir;
+
+    struct Point BaseLoc;
+    struct Point Loc;
+};
+
+struct Pacman
+{
+    int LivesC;
+
+    enum MDir CurrentDir;
+
+    struct Point BaseLoc;
+    struct Point Loc;
+};
+
+int main()
+{
+    int n,m;
+    scanf("%d%d", &n, &m);
+    for(int i=0;i<n;i++)
+        for (int j = 0 ; j<m;j++)
+            scanf("%c", &table[i][j]);
+    
 }
